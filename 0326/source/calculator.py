83ae6233513b27ade7531a296a76c03ca06113af
leo0204232 <leo90218@gmail.com>
2018-03-29 17:34:41 +0800

    0326

---
+++ 0326/source/calculator.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+def answer(a,b,f):
+    if f =='+':
+        return(a+b)
+    elif f == '-':
+        return(a-b)
+    elif f == '*':
+        return(a*b)
+    elif f == '/':
+        return(a/b)
+
